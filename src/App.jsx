5b6ac8cb89055419a51549700a09c6e4139ca39c
bo1atito <shorungbebolatito@gmail.com>
2025-06-21 02:18:37 +0100

    Background gradient effect added

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <main>
-        <div></div>
-        <div>
+      <main className='relative min-h-screen overflow-x-hidden'>
+        <div className='absolute -top-28 -left-28 h-[500px] w-[500px] -z-10 blur-[80px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full'></div>
+        <div className='overflow-x-hidden'>
           <Navbar />
           <Hero />
         </div>
